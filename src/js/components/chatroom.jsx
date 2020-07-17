10a2597fb9c229f0d9f2396e479b03e0c6bb5496
mvargasmoran <mvargasmoran@gmail.com>
2020-07-16 20:15:38 -0400

    Add starting hotpink theme
    Using styled components docs examples

--- src/js/components/chatroom.jsx
+++ src/js/components/chatroom.jsx
@@ -1,5 +1,9 @@
 import React, { useEffect, useState } from "react";
-import styled from "styled-components";
+import styled, { ThemeProvider } from "styled-components";
+
+const appBackground = styled.div`
+  background: rgba(250, 250, 250, 0.9);
+`;
 
 const MainContainer = styled.div`
   border: 1px solid tomato;
@@ -9,20 +13,22 @@ const MainContainer = styled.div`
   grid-template-areas:
     "messages sidebar"
     "textarea sidebar";
+  border: 2px solid hotpink;
+  box-sizing: border-box;
 `;
 
-const MessagesContainer = styled.div`
-  border: 1px solid peru;
+const MessagesContainer = styled(appBackground)`
+  border-bottom: 1px solid hotpink;
   grid-area: messages;
+  padding: 0 0.5em;
 `;
 
-const SidebarContainer = styled.div`
-  border: 1px solid hotpink;
+const SidebarContainer = styled(appBackground)`
+  border-left: 1px solid hotpink;
   grid-area: sidebar;
 `;
 
 const TextAreaContainer = styled.div`
-  border: 1px solid papayawhip;
   grid-area: textarea;
   display: grid;
   grid-template-columns: 4fr 1fr;
@@ -31,21 +37,80 @@ const TextAreaContainer = styled.div`
 const TextInput = styled.input`
   padding: 10px;
   font-size: 16px;
+  border: none;
+  outline: none;
+  margin-bottom: 1px;
 `;
 
 const Button = styled.button`
   padding: 10px;
-  border-radius: 4px;
+  border-radius: 0px;
+  color: white;
+  border: none;
+  transition: 0.3s all ease-in-out;
+  background-color: ${(props) => props.theme.backgroundColor};
+  border-radius: 0;
+  font-size: 16px;
+  &:hover {
+    background-color: ${(props) => props.theme.highlight};
+    background-position-x: 70px;
+  }
+  ,
+  &:focus {
+    background-color: ${(props) => props.theme.highlight};
+    background-position-x: -70px;
+  }
 `;
 
 const UserList = styled.ul`
-  border: 1px solid lime;
+  border-bottom: 1px solid ${(props) => props.theme.secondary};
   list-style-type: none;
+  padding-inline-start: 0;
 `;
 
 const UserItem = styled.li`
-  padding: 5px 0;
+  padding: 0.5em 0;
+  &::before {
+    content: "⚡️";
+    margin: 0 10px;
+  }
+  &.offline {
+    &::before {
+      content: "😴";
+      margin: 0 10px;
+    }
+  }
+  &.afk {
+    &::before {
+      content: "😴";
+      margin: 0 10px;
+    }
+  }
 `;
+
+const theme = {
+  backgroundColor: "hotpink",
+  highlight: "deeppink",
+  secondary: "hotpink",
+  secondaryHighlight: "deeppink",
+};
+
+Button.defaultProps = {
+  theme: {
+    backgroundColor: "tomato",
+    highlight: "coral",
+    secondary: "lime",
+    secondaryHighlight: "lawngreen",
+  },
+};
+UserList.defaultProps = {
+  theme: {
+    backgroundColor: "tomato",
+    highlight: "coral",
+    secondary: "lime",
+    secondaryHighlight: "lawngreen",
+  },
+};
 
 const Chatroom = ({ db }) => {
   const [userList, setUserList] = useState([]);
@@ -62,15 +127,19 @@ const Chatroom = ({ db }) => {
     <MainContainer>
       <MessagesContainer>Los mensajes</MessagesContainer>
       <SidebarContainer>
-        <UserList>
-          {userList.map((user) => (
-            <UserItem>{user.id}</UserItem>
-          ))}
-        </UserList>
+        <ThemeProvider theme={theme}>
+          <UserList>
+            {userList.map((user) => (
+              <UserItem>{user.id}</UserItem>
+            ))}
+          </UserList>
+        </ThemeProvider>
       </SidebarContainer>
       <TextAreaContainer>
         <TextInput />
-        <Button>Enviar</Button>
+        <ThemeProvider theme={theme}>
+          <Button>Enviar</Button>
+        </ThemeProvider>
       </TextAreaContainer>
     </MainContainer>
   );
